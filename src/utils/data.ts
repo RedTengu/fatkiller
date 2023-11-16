b23f1af4a14beeb373e5f1ad892500571eef4c4a
Red Yakuza <muratborokov@gmail.com>
2023-11-16 15:10:23 +0300

    fix: доработка начальных данных.

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,22 +1,24 @@
 type dataType = {
   username: string,
   email: string,
+  age: number,
   gender: string,
   height: number,
+  weight: number,
   objective: string,
   weightDiary: {
-    id: number,
+    id: string,
     weight: number,
     date: string
   }[],
   kcalDiary: {
-      id: number,
+      id: string,
       name: string,
       kcal: number,
       date: string
   }[],
   workoutDiary:{
-      id: number,
+      id: string,
       name: string,
       burnedKcal: number,
       date: string
@@ -26,41 +28,43 @@ type dataType = {
 export const data: dataType = {
   username: 'Мурат',
   email: 'mb@gmail.com',
+  age: 28,
   gender: 'M',
   height: 183,
-  objective: 'Похудение',
+  weight: 84,
+  objective: 'loss',
   weightDiary: [
     {
-      id: 1,
+      id: 'yNhMusf8nweB12p0RgYhv',
       weight: 78,
       date: '10.11.2023 10:05'
     },
     {
-      id: 2,
+      id: 'm1zVmCH90TASytQRahazN',
       weight: 79,
       date: '09.11.2023 09:55'
     },
     {
-      id: 3,
+      id: 'dQsZA9BQTYNH7Ux71_Axc',
       weight: 80,
       date: '08.11.2023 10:01'
     }
   ],
   kcalDiary: [
     {
-      id: 1,
+      id: '9YIddkTNN9EMCCXJEaSf7',
       name: 'Ужин',
       kcal: 600,
       date: '10.11.2023 17:30'
     },
     {
-      id: 2,
+      id: 'FRNVGtxYrP0uOvIZuhkId',
       name: 'Обед',
       kcal: 210,
       date: '10.11.2023 13:10'
     },
     {
-      id: 3,
+      id: 'DeE9q9I8IBtUm6ZKXZzaL',
       name: 'Завтрак',
       kcal: 200,
       date: '10.11.2023 10:02'
@@ -68,7 +72,7 @@ export const data: dataType = {
   ],
   workoutDiary: [
     {
-      id: 1,
+      id: 'fAlEYW_VOT7EoLxmEJ5',
       name: 'Бег',
       burnedKcal: 150,
       date: '10.11.2023 16:00'
